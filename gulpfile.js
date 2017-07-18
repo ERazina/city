df663e580563a1c9abd29e2b985dd87ddf27fed5
Elina <razina_elina@mail.ru>
2017-07-18 09:39:11 +0300

    пересобран сборщик pug, js, img

--- gulpfile.js
+++ gulpfile.js
@@ -1,47 +1,89 @@
 "use strict";
 
 let gulp = require('gulp'),
-    scss = require('gulp-sass'),
-    autoprefixer = require('gulp-autoprefixer'),
-    browserSync = require('browser-sync').create();
+	scss = require('gulp-sass'),
+	autoprefixer = require('gulp-autoprefixer'),
+	pug = require('gulp-pug'),
+	plumber = require('gulp-plumber'),
+	imagemin = require('gulp-imagemin'),
+	pngquant = require('imagemin-pngquant'),
+	browserSync = require('browser-sync').create();
 
 // Browser-sync
 gulp.task('browser-sync', function() {
-    browserSync.init({
-        server: {
-            baseDir: "build"
-        }
-    });
+	browserSync.init({
+		server: {
+			baseDir: "build"
+		}
+	});
+	gulp.watch('src/pug/*.pug', ['html']);
+	gulp.watch('src/sass/*.scss', ['scss']);
+	gulp.watch('src/js/*.js', ['js']);
+	gulp.watch('src/img/*.*', ['img']);
 
-    gulp.watch('src/*.html', ['html']);
-    gulp.watch('src/sass/*.scss', ['scss']);
+});
+// чистка папки build
+gulp.task('clean', function (cb) {
+	cleaner(build, cb);
 });
 
-// HTML
+//JADE
 gulp.task('html', function() {
-    gulp.src('src/*.html')
-    .pipe(gulp.dest('./build'))
+	gulp.src('src/pug/*.pug')
+		.pipe(plumber())
+		.pipe(pug({
+			pretty: true
+		}))
+		.pipe(gulp.dest('build'));
+});
+
+// Сжатие javascript
+gulp.task('js', function() {
+	return gulp.src('src/js/*.js')
+		.pipe(plumber())
+		// .pipe(uglyjs())
+		// .pipe(rename(function (path) {
+		//   path.basename += ".min"
+		// }))
+		.pipe(gulp.dest('build/js'));
+});
+
+// Сжатие изображений
+gulp.task('img', function() {
+	return gulp.src('src/img/*.*')
+		.pipe(plumber())
+		.pipe(imagemin({
+			progressive: true,
+			svgoPlugins: [{removeViewBox: false}],
+			use: [pngquant()]
+		}))
+		.pipe(gulp.dest('build/img'));
 });
 
 // SCSS
 gulp.task('scss', function() {
-    gulp.src('src/sass/*.scss')
-        .pipe(scss().on('error', function(error) {
-            console.log(error);
-        }))
-        .pipe(autoprefixer({
-            browsers: ['last 12 versions'],
-            cascade: false
-        }))
-        .pipe(gulp.dest('build/css'));
+	gulp.src('src/sass/*.scss')
+		.pipe(scss().on('error', function(error) {
+			console.log(error);
+		}))
+		.pipe(autoprefixer({
+			browsers: ['last 12 versions'],
+			cascade: false
+		}))
+		.pipe(gulp.dest('build/css'));
 });
 
 gulp.task('default', function() {
+	gulp.run('clean');
 	gulp.run('html');
-    gulp.run('scss');
-    gulp.run('browser-sync');
+	gulp.run('img');
+	gulp.run('scss');
+	gulp.run('js');
+	gulp.run('browser-sync');
 
 
-    gulp.watch('build/*.html').on('change', browserSync.reload);
-    gulp.watch('build/css/*.css').on('change', browserSync.reload);
+	gulp.watch('build/*.html').on('change', browserSync.reload);
+	gulp.watch('build/img/*.*').on('change', browserSync.reload);
+	gulp.watch('build/js/*.js').on('change', browserSync.reload);
+	gulp.watch('build/css/*.css').on('change', browserSync.reload);
 });
---
+++ src/js/script.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+	function validate(){
+		$('input#name, input#email').unbind().blur( function(){
+			var id = $(this).attr('id');
+			var val = $(this).val();
+
+			switch(id){
+				case 'email':
+					var emailRegexp = /^([a-zA-Z0-9_.-])+@([a-zA-Z0-9_.-])+\.([a-zA-Z])+([a-zA-Z])+/;
+
+					if( val.match(emailRegexp)){
+						$(this).addClass('noErr');
+					}
+					else{
+						$(this).addClass('err');
+					}
+				case 'name':
+					var nameRegexp = /^[a-zA-Zа-яА-Я]+$/;
+					if(val.match(nameRegexp)){
+						$(this).addClass('noErr');
+					}
+					else{
+						$(this).addClass('err');
+					}
+			}
+
+		});
+	}
+
+	validate();
+
+	});
